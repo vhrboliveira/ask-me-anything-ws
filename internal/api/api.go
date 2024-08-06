5fbe2abb2b3564560558fef0e95f4f2e6c0e6705
Vitor Oliveira <vhrb.oliveira@gmail.com>
2024-08-06 20:32:31 +0100

    feat: add createRoomMessage API

--- internal/api/api.go
+++ internal/api/api.go
@@ -188,6 +188,54 @@ func (h apiHandler) notifyClient(msg Message) {
 
 func (h apiHandler) getRooms(w http.ResponseWriter, r *http.Request) {}
 func (h apiHandler) createRoomMessage(w http.ResponseWriter, r *http.Request) {
+	rawRoomID := chi.URLParam(r, "room_id")
+	roomID, err := uuid.Parse(rawRoomID)
+
+	if err != nil {
+		http.Error(w, "invalid room id", http.StatusBadRequest)
+		return
+	}
+
+	_, err = h.q.GetRoom(r.Context(), roomID)
+	if err != nil {
+		if errors.Is(err, pgx.ErrNoRows) {
+			http.Error(w, "room not found", http.StatusBadRequest)
+			return
+		}
+
+		slog.Error("error getting room", "error", err)
+		http.Error(w, "internal server error", http.StatusInternalServerError)
+		return
+	}
+
+	type _body struct {
+		Message string `json:"message"`
+	}
+
+	var body _body
+	if err := json.NewDecoder(r.Body).Decode(&body); err != nil {
+		http.Error(w, "invalid body", http.StatusBadRequest)
+		return
+	}
+
+	messageID, err := h.q.InsertMessage(r.Context(), pgstore.InsertMessageParams{
+		RoomID:  roomID,
+		Message: body.Message,
+	})
+
+	if err != nil {
+		slog.Error("error inserting message", "error", err)
+		http.Error(w, "internal server error", http.StatusInternalServerError)
+		return
+	}
+
+	type response struct {
+		ID string `json:"id"`
+	}
+
+	data, _ := json.Marshal(response{ID: messageID.String()})
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(data)
 }
 
 func (h apiHandler) getRoomMessages(w http.ResponseWriter, r *http.Request)         {}
