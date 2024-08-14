1d2a66e6f6278ae5a5a50cb93cafcff1f54806aa
Vitor Oliveira <vhrb.oliveira@gmail.com>
2024-08-14 19:25:04 +0100

    feat: move shared read room code to a utils file

--- internal/api/utils.go
+++ internal/api/utils.go
@@ -1,3 +1,26 @@
+func (h apiHandler) readRoom(w http.ResponseWriter, r *http.Request) (room pgstore.GetRoomRow, rawRoomID string, roomId uuid.UUID, ok bool) {
+	rawRoomID = chi.URLParam(r, "room_id")
+	roomId, err := uuid.Parse(rawRoomID)
+	if err != nil {
+		http.Error(w, "invalid room id", http.StatusBadRequest)
+		return
+	}
+
+	room, err = h.q.GetRoom(r.Context(), roomId)
+	if err != nil {
+		if errors.Is(err, pgx.ErrNoRows) {
+			http.Error(w, "room not found", http.StatusBadRequest)
+			return pgstore.GetRoomRow{}, "", uuid.UUID{}, false
+		}
+
+		slog.Error("error getting room", "room", roomId, "error", err)
+		http.Error(w, "internal server error", http.StatusInternalServerError)
+		return pgstore.GetRoomRow{}, "", uuid.UUID{}, false
+	}
+
+	return room, rawRoomID, roomId, true
+}
+
 func sendJSON(w http.ResponseWriter, rawData any) {
 	data, _ := json.Marshal(rawData)
 
