82d89f17260d1d47f5464426f68050e6044ff420
Vitor Oliveira <vhrb.oliveira@gmail.com>
2024-09-20 20:41:53 +0100

    refactor: update generated models file

--- internal/store/pgstore/models.go
+++ internal/store/pgstore/models.go
@@ -28,11 +28,14 @@ type Room struct {
 }
 
 type User struct {
-	ID           uuid.UUID        `db:"id" json:"id"`
-	Email        string           `db:"email" json:"email"`
-	PasswordHash string           `db:"password_hash" json:"password_hash"`
-	Name         string           `db:"name" json:"name"`
-	Bio          string           `db:"bio" json:"bio"`
-	CreatedAt    pgtype.Timestamp `db:"created_at" json:"created_at"`
-	UpdatedAt    pgtype.Timestamp `db:"updated_at" json:"updated_at"`
+	ID             uuid.UUID        `db:"id" json:"id"`
+	Email          string           `db:"email" json:"email"`
+	Name           string           `db:"name" json:"name"`
+	CreatedAt      pgtype.Timestamp `db:"created_at" json:"created_at"`
+	UpdatedAt      pgtype.Timestamp `db:"updated_at" json:"updated_at"`
+	AvatarUrl      string           `db:"avatar_url" json:"avatar_url"`
+	EnablePicture  bool             `db:"enable_picture" json:"enable_picture"`
+	Provider       string           `db:"provider" json:"provider"`
+	ProviderUserID string           `db:"provider_user_id" json:"provider_user_id"`
+	NewUser        bool             `db:"new_user" json:"new_user"`
 }
